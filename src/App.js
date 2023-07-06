cefec30337aa54921c4b89c71c934e7799788c25
student <you@example.com>
2023-07-06 09:01:28 +0300

    Class Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Login from"./Login";
+import Products from './Products'
+
+function App() {
+  return (
+    <div>
+     <Login/>
+     <Products/>
+    </div>
+  );
+}
+
+export default App;
